610c758f71ac7e2ce51abc6b21a039ddfe7451bd
Unique Jain <uniquejn3004@gmail.com>
2025-08-18 17:10:14 +0530

    Delete My_Drive directory

--- My_Drive/src/db/db.js
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-
-const client = createClient({
-  url: "file:./db_data/app.db",
-});
-
-export const db = drizzle(client);
